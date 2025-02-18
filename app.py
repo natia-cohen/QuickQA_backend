f1ba5ad500e078e8b97cd6df32e8df1e6af0dd21
natiapa <natiapanosian@gmail.com>
2025-02-18 20:06:26 +0200

    Updated backend logic and fixed pycache tracking

--- app.py
+++ app.py
@@ -6,20 +6,16 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-from dotenv import load_dotenv
+
 
 from api.questions.question_routes import question_bp
 from api.answers.answer_routes import answer_bp
 
-
-load_dotenv()
-
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:*", "http://localhost:*"]}})
 
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:5000", "http://127.0.0.1:8000"]}})
-
-socketio = SocketIO(app, cors_allowed_origins="*")
+# socketio = SocketIO(app, cors_allowed_origins="*")
 
 app.config["MONGO_URI"] = os.getenv("MONGO_URI", "mongodb://localhost:27017/realtime_qa")
 mongo = PyMongo(app)
@@ -30,4 +26,4 @@ app.register_blueprint(question_bp)
 app.register_blueprint(answer_bp)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+   app.run(debug=True)
