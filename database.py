01689cfad8902bb11a8c09a6ae5b7acd1c061dd2
natiapa <natiapanosian@gmail.com>
2025-02-18 19:24:04 +0200

    Fixed database connection and removed unnecessary pycache files

--- database.py
+++ database.py
@@ -1,14 +1,32 @@
+import json
 from pymongo import MongoClient
 from config import Config
 
 try:
     client = MongoClient(Config.MONGO_URI, serverSelectionTimeoutMS=5000)  
     db = client["qa_forum"]
-
-
     client.admin.command("ping")
     print("MongoDB Connected Successfully!")
 
 except Exception as e:
-    print(f"Error connecting to MongoDB: {e}")
-    db = None
+    print(f" Error connecting to MongoDB: {e}")
+    db = None  
+
+def seed_database():
+    if db is not None:
+
+        collection = db["questions"]
+
+        try:
+            with open("questions.json", "r", encoding="utf-8") as file:
+                data = json.load(file)
+                if data:
+                    collection.insert_many(data)
+                    print(" Database seeded successfully!")
+                else:
+                    print("No data found in questions.json")
+        except Exception as e:
+            print(f"Error inserting data: {e}")
+
+if __name__ == "__main__":
+    seed_database()
