e2df9dbd2c20af91bbfe8b6697691209d5d51707
natiapa <natiapanosian@gmail.com>
2025-02-18 18:53:48 +0200

    Updated backend structure

--- app.py
+++ app.py
@@ -6,18 +6,20 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-from dotenv import load_dotenv
+
 
 from api.questions.question_routes import question_bp
 from api.answers.answer_routes import answer_bp
 
 
-load_dotenv()
+
 
 app = Flask(__name__)
 
 
-CORS(app, resources={r"/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:5000", "http://127.0.0.1:8000"]}})
+CORS(app, resources={r"/*": {"origins": ["http://127.0.0.1:*", "http://localhost:*"]}})
+
+
 
 # socketio = SocketIO(app, cors_allowed_origins="*")
 
